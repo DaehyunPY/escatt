83f207e0f7d8b5060400dea61545d63dee5bc1b6
Hyuna917 <hyuna917@me.com>
2015-05-23 16:27:06 +0900

    set limit basis

--- input.f90
+++ input.f90
@@ -1,9 +1,12 @@
 module input
     implicit none
-!     real(8), parameter :: Ra = 500.d0 
-    real(8), parameter :: Ra = 20.d0 
-    integer, parameter :: CooN = 5000
+    real(8), parameter :: Ra = 100.d0 
+    integer, parameter :: CooN = 1000
     real(8), parameter :: dr = Ra/dble(CooN)
+
+    real(8), parameter :: Rb = 20.d0 
+    integer, parameter :: CooK = int(Rb/dr)
+
     real(8) H(1:CooN, 1:CooN), E(1:CooN)
     integer,       parameter :: file_psi = 101,     file_energy = 102,    file_ana = 103
     character(30), parameter :: form_psi = '(6ES)', form_energy = '(ES)', form_ana = '(6ES)'
@@ -22,46 +25,74 @@ function CooR(i)
 end function CooR
 
 
-function nabla_r_close_open(dx, ii, jj)
-    real(8), intent( in) :: dx
-    integer, intent( in) :: ii, jj
-    real(8) :: nabla_r_close_open, i, j
-    real(8), parameter :: pi = 2.0d0*acos(0.0d0)
-    i = dble(ii)
-    j = dble(jj)
-    if( ii /= jj ) then 
-        nabla_r_close_open = &
-            (pi**2*((i-j)**3*(j+i)**2*sin(pi*(j+i))-sin(pi*(i-j))*(i-j)**2*(j+i)**3) &
-            -2*(i-j)**3*sin(pi*(j+i))+pi*(2*(i-j)**3*(j+i)*cos(pi*(j+i)) &
-            -2*cos(pi*(i-j))*(i-j)*(j+i)**3)+2*sin(pi*(i-j))*(j+i)**3) &
-            /(pi*dx**2*(i-j)**3*(j+i)**3)
-    else
-        nabla_r_close_open = &
-            -(-3*pi**2*(j+i)**2*sin(pi*(j+i))+6*sin(pi*(j+i))-6*pi*(j+i)*cos(pi*(j+i)) &
-            +pi**3*(j+i)**3)/(pi*dx**2*(j+i)**3)/3.d+0
-    end if
-end function nabla_r_close_open
-function nabla_r_close_close(dx, ii, jj)
+function nabla_limit(dx, kk, ll, ii, jj)
     real(8), intent(in) :: dx
-    integer, intent(in) :: ii, jj
-    real(8), parameter :: pi = 2.0d0*acos(0.0d0)
-    real(8) nabla_r_close_close, i, j
+    real(8), parameter  :: pi = 2.0d0*acos(0.0d0)
+    integer, intent(in) :: ii, jj, kk, ll
+    real(8)  :: nabla_limit, i, j, k, l, N
+    real(16) :: tmp 
+    k = dble(kk)
+    l = dble(ll)
     i = dble(ii)
     j = dble(jj)
+    N = 10.d0**5 
     if( ii /= jj ) then 
-        nabla_r_close_close = &
-!           -(-1.d0)**(i-j)/dx**2.d0 *(2.d0/(i-j)**2.d0 -2.d0/(i+j)**2.d0)
-            (pi**2*((i-j)**3*(j+i)**2*sin(pi*(j+i))-sin(pi*(i-j))*(i-j)**2*(j+i)**3) &
-            -2*(i-j)**3*sin(pi*(j+i))+2*pi*(i-j)**3*(j+i)*cos(pi*(j+i)) &
-            -2*pi*cos(pi*(i-j))*(i-j)*(j+i)**3+2*sin(pi*(i-j))*(j+i)**3) &
-            /(pi*dx**2*(i-j)**3*(j+i)**3)
+        tmp = &
+!             (((4*pi**2*l**2-4*pi**2*l+pi**2)*cos((j+i)*(2*pi*l+5*pi)/k/2.d+0) &
+!             +(-20*pi**2*l**2+12*pi**2*l+3*pi**2)*cos((j+i)*(2*pi*l+3*pi)/k/2.d+0) &
+!             +(40*pi**2*l**2-8*pi**2*l-14*pi**2)*cos((j+i) &
+!             *(2*pi*l+pi)/k/2.d+0)+(-40*pi**2*l**2-8*pi**2*l+14*pi**2)*cos((j+i) &
+!             *(2*pi*l-pi)/k/2.d+0)+(20*pi**2*l**2+12*pi**2*l-3*pi**2) &
+!             *cos((j+i)*(2*pi*l-3*pi)/k/2.d+0)+(-4*pi**2*l**2-4*pi**2*l-pi**2) &
+!             *cos((j+i)*(2*pi*l-5*pi)/k/2.d+0))/(8*cos(3*pi*(j+i)/k) &
+!             *k**2-48*cos(2*pi*(j+i)/k)*k**2+120*cos(pi*(j+i)/k)*k**2-80*k**2) &
+!             -((4*pi**2*l**2-4*pi**2*l+pi**2)*cos((i-j)*(2*pi*l+5*pi)/k/2.d+0) &
+!             +(-20*pi**2*l**2+12*pi**2*l+3*pi**2)*cos((i-j)*(2*pi*l+3*pi)/k/2.d+0) &
+!             +(40*pi**2*l**2-8*pi**2*l-14*pi**2)*cos((i-j)*(2*pi*l+pi)/k/2.d+0) &
+!             +(-40*pi**2*l**2-8*pi**2*l+14*pi**2)*cos((i-j)*(2*pi*l-pi)/k/2.d+0) &
+!             +(20*pi**2*l**2+12*pi**2*l-3*pi**2)*cos((i-j)*(2*pi*l-3*pi)/k/2.d+0) &
+!             +(-4*pi**2*l**2-4*pi**2*l-pi**2)*cos((i-j)*(2*pi*l-5*pi)/k/2.d+0)) &
+!             /(8*cos(3*pi*(i-j)/k)*k**2-48*cos(2*pi*(i-j)/k)*k**2+120 &
+!             *cos(pi*(i-j)/k)*k**2-80*k**2))/(dx**2*l)
+            (((4*pi**2*N**2-4*pi**2*N+pi**2)*cos((2*pi*(j+i)*N+5*pi*(j+i))/k/2.d+0) &
+            +(-20*pi**2*N**2+12*pi**2*N+3*pi**2)*cos((2*pi*(j+i)*N &
+            +3*pi*(j+i))/k/2.d+0)+(40*pi**2*N**2-8*pi**2*N-14*pi**2) &
+            *cos((2*pi*(j+i)*N+pi*(j+i))/k/2.d+0)+(-40*pi**2*N**2-8*pi**2*N+14*pi**2) &
+            *cos((2*pi*(j+i)*N-pi*(j+i))/k/2.d+0)+(20*pi**2*N**2 &
+            +12*pi**2*N-3*pi**2)*cos((2*pi*(j+i)*N-3*pi*(j+i))/k/2.d+0) &
+            +(-4*pi**2*N**2-4*pi**2*N-pi**2)*cos((2*pi*(j+i)*N-5*pi*(j+i))/k/2.d+0)) &
+            /(8*cos(3*pi*(j+i)/k)*k**2-48*cos(2*pi*(j+i)/k)*k**2 &
+            +120*cos(pi*(j+i)/k)*k**2-80*k**2)-((4*pi**2*N**2-4*pi**2*N+pi**2) &
+            *cos((2*pi*(i-j)*N+5*pi*(i-j))/k/2.d+0) &
+            +(-20*pi**2*N**2+12*pi**2*N+3*pi**2)*cos((2*pi*(i-j)*N+3*pi*(i-j))/k/2.d+0) &
+            +(40*pi**2*N**2-8*pi**2*N-14*pi**2)*cos((2*pi*(i-j)*N+pi*(i-j))/k/2.d+0) &
+            +(-40*pi**2*N**2-8*pi**2*N+14*pi**2)*cos((2*pi*(i-j)*N-pi*(i-j))/k/2.d+0) &
+            +(20*pi**2*N**2+12*pi**2*N-3*pi**2)*cos((2*pi*(i-j)*N-3*pi*(i-j))/k/2.d+0)+( &
+            -4*pi**2*N**2-4*pi**2*N-pi**2)*cos((2*pi*(i-j)*N-5*pi*(i-j))/k/2.d+0)) &
+            /(8*cos(3*pi*(i-j)/k)*k**2-48*cos(2*pi*(i-j)/k)*k**2+120*cos(pi*(i-j)/k)*k**2-80*k**2))/(dx**2*l)
     else
-        nabla_r_close_close = &
-!           -(-1.d0)**(i-j)/dx**2.d0 *(pi**2.d0/3.d0 -1/2.d0/i**2.d0) 
-            (3*pi**2*(j+i)**2*sin(pi*(j+i))-6*sin(pi*(j+i))+6*pi*(j+i)*cos(pi*(j+i)) &
-            -pi**3*(j+i)**3)/(pi*dx**2*(j+i)**3)/3.d+0
+        tmp = &
+!             (((4*pi**2*l**2-4*pi**2*l+pi**2)*cos((j+i)*(2*pi*l+5*pi)/k/2.d+0) &
+!             +(-20*pi**2*l**2+12*pi**2*l+3*pi**2)*cos((j+i)*(2*pi*l+3*pi)/k/2.d+0) &
+!             +(40*pi**2*l**2-8*pi**2*l-14*pi**2)*cos((j+i)*(2*pi*l+pi)/k/2.d+0) &
+!             +(-40*pi**2*l**2-8*pi**2*l+14*pi**2)*cos((j+i)*(2*pi*l-pi)/k/2.d+0) &
+!             +(20*pi**2*l**2+12*pi**2*l-3*pi**2)*cos((j+i)*(2*pi*l-3*pi)/k/2.d+0) &
+!             +(-4*pi**2*l**2-4*pi**2*l-pi**2)*cos((j+i)*(2*pi*l-5*pi)/k/2.d+0)) &
+!             /(8*cos(3*pi*(j+i)/k)*k**2-48*cos(2*pi*(j+i)/k)*k**2 &
+!             +120*cos(pi*(j+i)/k)*k**2-80*k**2)+pi**2*(-(2*l**3+3*l**2+l)/6.d+0 &
+!             +(l**2+l)/2.d+0-l/4.d+0)/k**2)/(dx**2*l)
+            (((4*pi**2*N**2-4*pi**2*N+pi**2)*cos((2*pi*(j+i)*N+5*pi*(j+i))/k/2.d+0) &
+            +(-20*pi**2*N**2+12*pi**2*N+3*pi**2)*cos((2*pi*(j+i)*N+3*pi*(j+i))/k/2.d+0) &
+            +(40*pi**2*N**2-8*pi**2*N-14*pi**2) &
+            *cos((2*pi*(j+i)*N+pi*(j+i))/k/2.d+0)+(-40*pi**2*N**2-8*pi**2*N+14*pi**2) &
+            *cos((2*pi*(j+i)*N-pi*(j+i))/k/2.d+0)+(20*pi**2*N**2+12*pi**2*N-3*pi**2) &
+            *cos((2*pi*(j+i)*N-3*pi*(j+i))/k/2.d+0) &
+            +(-4*pi**2*N**2-4*pi**2*N-pi**2)*cos((2*pi*(j+i)*N-5*pi*(j+i))/k/2.d+0)) &
+            /(8*cos(3*pi*(j+i)/k)*k**2-48*cos(2*pi*(j+i)/k)*k**2 &
+            +120*cos(pi*(j+i)/k)*k**2-80*k**2)+pi**2*(-(2*N**3+3*N**2+N)/6.d+0+(N**2+N)/2.d+0-N/4.d+0)/k**2)/(dx**2*l)
     end if
-end function nabla_r_close_close
+    nabla_limit = tmp 
+end function nabla_limit
 
 
 function f_ana(ty, r)
--- main.f90
+++ main.f90
@@ -6,19 +6,18 @@ contains
 
 subroutine proc1
     integer i, j
-
-    H(:, :) = 0.d0  
+    write(*, *) "CooN Number check.", Rb/dr, int(Rb/dr) 
+    H(:, :) = 0.d0 
     do j = 1, CooN
-    do i = 1, CooN
-!         H(i, j) = nabla_r_close_open(dr, i, j)
-        H(i, j) = nabla_r_close_close(dr, i, j)
-    enddo
+        do i = 1, CooN
+            H(i, j) = nabla_limit(dr, CooK, CooN, i, j)
+        enddo
     enddo
     do i = 1, CooN
         H(i, i) = H(i, i) +Poten(CooR(i))
     enddo
-
     H(:, :) = -0.5d0*H(:, :)
+    write(*, *) "Process 1 is done."
 end subroutine proc1
 
 
@@ -36,25 +35,22 @@ subroutine proc2
     character(1) :: jobz = 'Vectors', uplo = 'Upper' 
     integer lda, n, lwork, info
     real(8), allocatable :: work(:)
-
     allocate(work(1:3*CooN -1))
       lda = CooN
         n = CooN
     lwork = 3*n -1
      info = 0
-
 !   lwork = -1
 !   call dsyev(jobz, uplo, n, H(1:n, 1:n), lda, E(1:n), work, lwork, info)
 !   lwork = int(work(1))
-    
     call dsyev(jobz, uplo, n, H(1:n, 1:n), lda, E(1:n), work, lwork, info)
     if(info /= 0) stop "SUBROUTINE diag_real: Something is wrong. (2)"  
+    write(*, *) "Process 2 is done." 
 end subroutine proc2
 
 
 subroutine proc3 
     integer i, j 
-
     write(file_psi, form_psi) 0.d0, 0.d0, 0.d0, 0.d0, 0.d0, 0.d0 
     write(file_ana, form_ana) 0.d0, 0.d0, 0.d0, 0.d0  
     do i = 1, CooN 
@@ -62,7 +58,6 @@ subroutine proc3
         write(file_ana, form_ana) CooR(i), (f_ana(j, CooR(i))**2.d0, j = 1, 3)
         write(file_energy, form_energy) E(i)
     end do 
-    
     write(*, '(10F10.3)') (E(i), i = 1, 10)
 end subroutine proc3
 end module process
@@ -71,16 +66,13 @@ end module process
 program main
     use process
     implicit none
-
     open(file_psi, file = "inout/psi.d")
     open(file_energy, file = "inout/energy.d")
     open(file_ana, file = "inout/ana.d")
-
     call proc1
-    call proc2_test 
+    call proc2_test
     call proc2
-    call proc3
-
+!     call proc3
     close(101)
     close(102)
     close(103)
